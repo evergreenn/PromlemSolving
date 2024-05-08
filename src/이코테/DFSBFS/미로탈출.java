ce7b626d5a94ba9184cd36076c6d36f5c7939432
ulsandonghun <donghoon141@naver.com>
2024-05-08 13:55:57 +0900

    Breadth First Search - short cost

---
+++ src/이코테/DFSBFS/미로탈출.java
@@ -0,0 +1,76 @@
+package 이코테.DFSBFS;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class 미로탈출 {
+    static int[][] arr;
+    static int[][] cost;
+    static int N;
+    static int M;
+    static int[] dx={-1,+1,0,0,};
+    static int[] dy={0,0,-1,+1};
+    static Queue<node> queue=new LinkedList<>();
+    public static void main(String[] args) {
+        // 1,1 N,M 으로 카운트
+        //BFS 는 가까운것을 우선적으로 탐색 = 최종 도착지점까지의 최단거리 구하기 가능
+        // 해당 값의 이전 cost 에서 1 더하기
+        Scanner s = new Scanner(System.in);
+
+         N=s.nextInt();
+         M=s.nextInt();
+         s.nextLine();
+
+        arr = new int[N][M];
+        cost=new int[N][M];
+        cost[0][0]=1;// 첫 방문지도 1회 이동으로 침.
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Arrays.stream(s.nextLine().split(""))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
+        }
+        bfs(0,0);
+        System.out.println(cost[N-1][M-1]);
+
+    }
+    public static void bfs(int x, int y) {
+        arr[x][y]=2;//방문처리
+        int nx,ny;
+        queue.add(new node(x,y));
+
+        while (!queue.isEmpty()) {
+            node cur = queue.remove();
+            arr[cur.x][cur.y] = 2;
+
+            x = cur.x;
+            y = cur.y;
+
+            for (int i = 0; i < 4; i++) {
+                nx = x + dx[i];
+                ny = y + dy[i];
+                if (nx < 0 || ny < 0 || nx >= N || ny >= M) {
+                    continue;
+                }
+                if (arr[nx][ny] == 0 || arr[nx][ny] == 2) {
+                    continue;
+                }
+                queue.add(new node(nx, ny));
+                cost[nx][ny]=cost[x][y]+1; //주의 큐에 넣을때, 최솟값 처리 해줘야 가까운 순서를 같은 레벨로 횟수가 처리됨.
+            }
+        }
+
+    }
+
+    static class node {
+        public node(int x, int y) {
+            this.x=x;
+            this.y=y;
+        }
+
+        int x;
+        int y;
+    }
+}
