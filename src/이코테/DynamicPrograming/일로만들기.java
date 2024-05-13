4716a1210b0c0e77800f17ff0f4c697fe537a4a5
ulsandonghun <donghoon141@naver.com>
2024-05-14 00:15:56 +0900

    DP - retry

--- src/이코테/DynamicPrograming/일로만들기.java
+++ src/이코테/DynamicPrograming/일로만들기.java
@@ -5,29 +5,33 @@ import java.util.Scanner;
 
 public class 일로만들기 {
     public static void main(String[] args) {
-        //11시 45분
+        //DP는 반복되는것을 찾기.
+        //DP는 무조건 바텀업이 1순위
 
         HashMap<Integer, Integer> map = new HashMap<>();
 
         Scanner s = new Scanner(System.in);
-        int[] arr=new int[300000];
+        int[] arr = new int[300000];
 
         int X = s.nextInt();
 
+        for (int i = 2; i <= X; i++) {
+            arr[i] = arr[i - 1] + 1;
 
-        while ( X!=1 ) {
-            if (X % 5 == 0) {
-                arr[X]=arr[X/5]+1;
-                X/=5;
-
-            } else if (X % 3 == 0) {
-                arr[X] = arr[X / 3] + 1;
-                X /= 3;
-            } else {
-                arr[X]=arr[X-1]+1;
-                X-=1;
+            if (i % 5 == 0) {
+                arr[i] = Math.min(arr[i / 5] + 1,arr[i]);
             }
+            if (i % 3 == 0) {
+                arr[i] = Math.min(arr[i / 3] + 1, arr[i]);
+            }
+            if (i % 2 == 0) {
+                arr[i] = Math.min(arr[i / 2] + 1, arr[i]);
+            }
+
+
+
         }
+
         System.out.println(arr[X]);
     }
 }
