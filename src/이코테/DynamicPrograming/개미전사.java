364d656cf1c03014b7829fb97c0c06bac3a2bc96
ulsandonghun <donghoon141@naver.com>
2024-05-14 23:47:09 +0900

    DP - retry

--- src/이코테/DynamicPrograming/개미전사.java
+++ src/이코테/DynamicPrograming/개미전사.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 
 public class 개미전사 {
     public static void main(String[] args) {
+        //dp -> 바텀 업
         Scanner s = new Scanner(System.in);
 
         int N = s.nextInt();
@@ -16,14 +17,13 @@ public class 개미전사 {
             arr[i]=s.nextInt();
         }
         dp[0]=arr[0];
-        dp[1]=arr[1];
+        dp[1]=Math.max(arr[1],dp[0]);// 핵심 아이디어 -> 만약 이전값을 그대로 사용할수 있다면
+        // 특히 그대로 사용하는 값이 더 크다면 해당 값을 최댓값으로 정하기.
+
         int maximum=Integer.MIN_VALUE;
         for (int i = 2; i < N; i++) {
-            for (int j = 0; j < i - 1; j++) {
-                maximum=Math.max(maximum,dp[j]);
-            }
-            dp[i]+=maximum+arr[i];
-            maximum=Integer.MIN_VALUE;
+            dp[i]=Math.max(dp[i-1],dp[i-2]+arr[i]);
+
         }
         System.out.println(Arrays.toString(dp));
         int max = Arrays.stream(dp)
