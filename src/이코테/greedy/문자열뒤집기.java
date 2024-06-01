f5ce14b6f60c040084b5e5e9919fe78030c1b400
ulsandonghun <donghoon141@naver.com>
2024-06-02 00:30:20 +0900

    [fix] : Greedy
    현재 글자에서 다음 글자로 변경되는 순간 카운트하는 아이디어로 변경

--- src/이코테/greedy/문자열뒤집기.java
+++ src/이코테/greedy/문자열뒤집기.java
@@ -9,40 +9,21 @@ public class 문자열뒤집기 {
         // 1 그룹과 0 그룹의 갯수를 카운트 하고, 두 그룹중 작은 그룹의 갯수를 반환
         // -> 답을 다시 보고 의역하자면 0을 1로 바꾸는 경우와 1을 0으로 바꾸는 경우중 작은 것을 반환
         Scanner s = new Scanner(System.in);
-        int zero=0;
-        int one=0;
+        int[] result = new int[2];
 
-        String[] input = s.nextLine().split("");
-
-        Queue<Integer> queue = new LinkedList<>();
-        for (int i = 0; i < input.length; i++) {
-            int n = Integer.valueOf(input[i]);
-            queue.add(n);
-        }
-        while (!queue.isEmpty()) {
-            int n = queue.remove();
-            if (n == 0) {
-                while (!queue.isEmpty()) {
-                    n=queue.peek();
-                    if (n != 0) {
-                        break;
-                    }
-                    queue.remove();
-                }
-                zero++;
-            } else {
-                while (!queue.isEmpty()) {
-                    n=queue.peek();
-                    if (n != 1) {
-                        break;
-                    }
-                    queue.remove();
-                }
-                one++;
+        String input = s.nextLine();
+        for (int i = 0; i < (input.length()-1); i++) {
+            char c=input.charAt(i);
+            if (input.charAt(i) != input.charAt(i+1)) {
+//                int n=  Integer.valueOf(c); 실수주의 , valueOf는 유니코드 값으로 바꿔버림
+                int n = Character.getNumericValue(c);
+                result[n]+=1;
             }
         }
+        System.out.println("result[0] = " + result[0]);
+        System.out.println("resuot[1] = " + result[1]);
 
-        System.out.println(Math.min(zero, one));
+        System.out.println(Math.min(result[0], result[1]));
 
     }
 }
