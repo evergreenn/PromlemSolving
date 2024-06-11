fb11ac4b1178b1c0c1f32813fd1c5aedd0fa854c
ulsandonghun <donghoon141@naver.com>
2024-06-12 00:29:50 +0900

    [fix] : 무지의 먹방 라이브
     linked list를 이용한 회전판 순회시 링크드 리스트의 인덱스 값이 음식 순번과 다르므로 오류 발생, 해당 클래스에 별도의 맴버변수 설정후 저장하는 방식으로 문제 해결

--- src/이코테/greedy/무지의먹방라이브.java
+++ src/이코테/greedy/무지의먹방라이브.java
@@ -3,7 +3,7 @@ package 이코테.greedy;
 import java.util.LinkedList;
 
 public class 무지의먹방라이브 {
-    //11:40
+    //11:40 ~ 12:00
     public static void main(String[] args) {
 
         int[] food_times={3, 1, 2};
@@ -12,8 +12,11 @@ public class 무지의먹방라이브 {
         LinkedList<Food> table=new LinkedList<Food>();
 
         for (int i = 0; i < food_times.length; i++) {
-            table.add(new Food(food_times[i]));
+            table.add(new Food(food_times[i],i));
         }
+        System.out.println("초깃값"+table.toString());
+
+
         int cnt=0;
         for (int i = 0; i < k; i++) {
             while (true) {
@@ -29,23 +32,26 @@ public class 무지의먹방라이브 {
                 table.add(table.removeFirst());
                 cnt++;
                 if (cnt == food_times.length) {
-                    break;
+                    System.out.println(-1) ;
                 }
             }
+            System.out.println(table.toString());
         }
         if (cnt != 0) {
             System.out.println(-1);
         } else {
+
             for (int i = 0; i < table.size(); i++) {
                 Food food = table.get(i);
 
                 if (food.time != 0) {
-                    System.out.println(i);
+                    System.out.println(food.num+1);
                 }
                 return;
             }
         }
         System.out.println(-1);
+
 
     }
 
@@ -53,9 +59,16 @@ public class 무지의먹방라이브 {
 }
 class Food{
     int time;
+    int num;
 
-    public Food(int time) {
+    public Food(int time, int num) {
         this.time=time;
+        this.num=num;
+    }
+
+    @Override
+    public String toString() {
+        return time+" ";
     }
 
 }
