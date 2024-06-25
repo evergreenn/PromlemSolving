8c90eefb68094265876ad74d53da2438d87aa515
ulsandonghun <donghoon141@naver.com>
2024-06-25 19:05:43 +0900

    String
    문자열 나누기 문제 풀이

---
+++ src/문자열/문자열나누기별해.java
@@ -0,0 +1,42 @@
+package 문자열;
+
+public class 문자열나누기별해 {
+    //2:44~7:03
+    public int  solution(String s) {
+
+        int cnt=0;
+        int init=s.charAt(0);
+        int init_cnt=0;
+        int next_cnt=0;
+        if(s.length()==1){
+            return 1;
+        }
+
+
+        for(int i=0;i<s.length();i++){
+
+            if(i==s.length()-1){
+                cnt++;
+                break;
+            }
+            char c= s.charAt(i);
+            if(init!=c){
+                next_cnt++;
+                if(init_cnt==next_cnt){
+                    cnt++;
+                    init=s.charAt(i+1);
+                    next_cnt=0;
+                    init_cnt=0;
+                }
+            }else{
+                init_cnt++;
+            }
+
+
+        }
+
+
+        int answer = cnt;
+        return answer;
+    }
+}
