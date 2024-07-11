eb7d36b5953b6fc9724b9343e44fe1dc5aa3724a
ulsandonghun <donghoon141@naver.com>
2024-07-12 01:12:43 +0900

    [fix] : Sorting
    항상 모든 인덱스의 가운데에 있으면 최솟값임을 이용/중앙값 처리, 인덱스는 n-1을 한 다음에 /2 를 해줘야 중앙값이 된다는 점을 유념

--- src/이코테/Sort/안테나.java
+++ src/이코테/Sort/안테나.java
@@ -9,7 +9,7 @@ import java.util.Collections;
 import java.util.Comparator;
 
 public class 안테나 {
-    //12:32 ~
+    //12:32 ~ 1:11
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -18,59 +18,9 @@ public class 안테나 {
         int[] input = Arrays.stream(br.readLine().split(space))
                 .mapToInt(Integer::parseInt)
                 .toArray();
-
-        int min=(int)1e9;
-        int t;
-        ArrayList<Node> list = new ArrayList<>();
-
-        for(int i=0;i<input.length;i++){
-            int target=input[i];
-            int sum=0;
-            for(int j=0;j<input.length;j++){
-                if(i==j){
-                    continue;
-                }
-                sum+=Math.abs(target-input[j]);
-            }
-            t=Math.min(min,sum);
-            if (t != min ) {
-                list.add(new Node(input[i],t));
-                min=t;
-            }
-        }
-
-//        Collections.sort(list,new Comparator<Node>(){
-//            @Override
-//            public int compare(Node o1,Node o2){
-//
-//                //오름차순 정렬
-//                int judge=o1.distance-o2.distance;
-//
-//                if (judge == 0) {
-//                    judge=o1.index-o2.index;
-//                    return judge;
-//                }
-//                return judge;
-//            }
-//        });
+        Arrays.sort(input);
+        System.out.println(input[(input.length-1)/2]);
 
 
-
-
-        System.out.println(list.get(0).index);
-
-
-    }
-    static class Node{
-        int index;
-        int distance;
-        public Node(int index,int distance){
-            this.index=index;
-            this.distance=distance;
-        }
-        @Override
-        public String toString(){
-            return String.valueOf(this.index)+" ";
-        }
     }
 }
