26ba0e150085e984baba5d722d7f22675300024f
ulsandonghun <donghoon141@naver.com>
2024-07-28 17:48:27 +0900

    [refactor] : 정렬된배열에서특정수의 개수 (2개의 이진탐색 차이)
    기존의 반복문을 통한 이진탐색 구현에서, 재귀함수 코드로 리팩토링

--- src/이코테/BinarySearch/정렬된배열에서특정수의개수구하기.java
+++ src/이코테/BinarySearch/정렬된배열에서특정수의개수구하기.java
@@ -5,11 +5,13 @@ import java.util.Scanner;
 
 public class 정렬된배열에서특정수의개수구하기 {
     static int[] input;
+    static int N;
+
     public static void main(String[] args) {
 
         Scanner s = new Scanner(System.in);
 
-        int N = s.nextInt();
+         N = s.nextInt();
         int x = s.nextInt();
         s.nextLine();
 
@@ -18,25 +20,27 @@ public class 정렬된배열에서특정수의개수구하기 {
                 .toArray();
         System.out.println(Arrays.toString(input));
 
-        System.out.println(upperbound(0, input.length,x )-
+        System.out.println(upperbound(0, input.length, x) -
                 lowerbound(0, input.length, x));
+
+        System.out.println("upperbound(0, input.length, x) = " + upperbound(0, input.length, x));
+        System.out.println("lowerbound(0, input.length, x) = " + lowerbound(0, input.length, x));
 
     }
 
-    public int binarysearch(int start,int end,int target){
+    public int binarysearch(int start, int end, int target) {
 
+        while (start <= end) {
 
-        while (start<end){
-
-           int mid=(start+end)/2;
-            if(input[mid]==target){
+            int mid = (start + end) / 2;
+            if (input[mid] == target) {
                 return mid;
             }
-            if(input[mid]<target){
-                start=mid+1;
+            if (input[mid] < target) {
+                start = mid + 1;
             }
-            if(input[mid]>target){
-                end=mid-1;
+            if (input[mid] > target) {
+                end = mid - 1;
             }
 
         }
@@ -44,40 +48,48 @@ public class 정렬된배열에서특정수의개수구하기 {
 
 
     }
-    public static int lowerbound(int start,int end,int target){
 
-        while(start<end){
+    public static int lowerbound(int start, int end, int target) {
+        int mid = (start + end) / 2;
 
-            int mid=(start+end)/2;
-            System.out.println("mid = " + mid);
-
-            if(input[mid]>=target){
-
-                end=mid;
-            }
-            else{
-                start=mid+1;
-            }
+        if (start > end) {
+            return -1;
         }
 
-        return end;
+        if (input[mid] == target && (mid == 0 || input[mid - 1] < target)) {
+
+            return mid;
+        }
+        if (input[mid] >= target) {
+            //왼쪽탐색
+            return lowerbound(start, mid - 1, target);
+        } else {
+           return lowerbound(mid + 1, end, target);
+        }
 
     }
-    public static int upperbound(int start,int end,int target){
 
-        while(start<end){
+    public static int upperbound(int start, int end, int target) {
+        int mid=(start+end)/2;
 
-
-            int mid=(start+end)/2;
-            System.out.println("mid = " + mid);
-            if(input[mid]>target){
-                end=mid;
-            }else{
-                start=mid+1;
-            }
-
+        if(start>end){
+            return -1;
         }
-        return end;
+
+        if(input[mid]==target && (mid ==N-1 ||input[mid+1]>target)){
+
+            return mid;
+        }
+
+        if(input[mid]>target){
+            //타겟보다 클 경우 왼쪽탐색
+           return upperbound(start,mid-1,target);
+
+        }else{
+
+            return upperbound(mid+1,end,target);
+        }
+
     }
 }
 /*
