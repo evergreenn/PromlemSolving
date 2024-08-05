4c7a55856a443c055fe89584a947b7602ad46ba8
ulsandonghun <donghoon141@naver.com>
2024-08-05 19:35:10 +0900

    [fix] BFS 최단거리 인접리스트

--- src/이코테/DFSBFS/특정거리의도시찾기2.java
+++ src/이코테/DFSBFS/특정거리의도시찾기2.java
@@ -1,12 +1,13 @@
 package 이코테.DFSBFS;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
 public class 특정거리의도시찾기2 {
-    static int[][] map;
+    //인접 리스트 방식 사용 -> 시작 노드를 좌표가 아닌 노드 번호로 생각 !
     static ArrayList<ArrayList<Integer>> list= new ArrayList<ArrayList<Integer>>();
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
@@ -15,10 +16,10 @@ public class 특정거리의도시찾기2 {
         int M = s.nextInt();
         int K = s.nextInt();
         int X = s.nextInt();
-        boolean[] visit = new boolean[N + 1];
        int[] dp = new int[N + 1];
         for (int i = 0; i <= N; i++) {
             list.add(new ArrayList<>());
+            dp[i]=-1;
         }
 
 
@@ -29,12 +30,10 @@ public class 특정거리의도시찾기2 {
             list.get(x).add(y);
         }
         //첫 시작 노드
-        Integer first = list.get(X).get(0);
 
         Queue<Integer> queue = new LinkedList<>();
 
         queue.add(X);
-        visit[X]=true;
         dp[X]=0;
 
         ArrayList<Integer> answer = new ArrayList<>();
@@ -44,12 +43,11 @@ public class 특정거리의도시찾기2 {
             for (int i = 0; i < list.get(node).size(); i++) {
                 Integer adjcent = list.get(node).get(i);
 
-                if (!visit[adjcent]) {
+                if (dp[adjcent]==-1) {
                     dp[adjcent]=dp[node]+1;
                     if (dp[adjcent] == K) {
                         answer.add(adjcent);
                     }
-                    visit[adjcent]=true;
                     queue.add(adjcent);
                 }
             }
@@ -58,18 +56,13 @@ public class 특정거리의도시찾기2 {
 
         if (answer.size() == 0) {
             System.out.println(-1);
-
         }else{
+            Collections.sort(answer);
 
             for (int i : answer) {
                 System.out.println(i);
             }
         }
-
-    }
-    public static void BFS(int X){
-
-
 
     }
 }
